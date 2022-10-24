833faea20814d4716cc2125a0342550b1db99497
DavidHunterScot <github@davidhunter.scot>
2022-10-24 21:52:01 +0100

    Added first tool which finds urls in a string and outputs them as links that open in a new tab. Reason behind this tool is I wanted to extract URLs in a CSS to make accessing assets easier. Hopefully this tool is worth its development efforts.

---
+++ src/assets/javascript/tools.js
@@ -0,0 +1,54 @@
+var txtFindUrlsInString = document.getElementById( 'txtFindUrlsInString' );
+var excludeTrailingBracketFindUrlsInString = document.getElementById( 'excludeTrailingBracketFindUrlsInString' );
+var btnFindUrlsInString = document.getElementById( 'btnFindUrlsInString' );
+var resultFindUrlsInString = document.getElementById( 'resultFindUrlsInString' );
+
+/*btnFindUrlsInString.addEventListener( 'click', function()
+{
+  resultFindUrlsInString.innerHTML = 'Result...<br>' + extractUrls( txtFindUrlsInString.value );
+});*/
+
+btnFindUrlsInString.addEventListener( 'click', function()
+{
+  var urls = extractUrls( txtFindUrlsInString.value, excludeTrailingBracketFindUrlsInString.checked );
+  
+  if( urls.length > 0 )
+  {
+    for( var u = 0; u < urls.length; u++ )
+    {
+      resultFindUrlsInString.innerHTML += '<a href="' + urls[ u ] + '" target="_blank">' + urls[ u ] + '</a><br>';
+      
+      /*if( u < urls.length - 1 )
+        resultFindUrlsInString.innerHTML += '<br>';
+      }*/
+    }
+  }
+  else
+  {
+    resultFindUrlsInString.innerHTML = 'No URLs found.';
+  }
+});
+
+function extractUrls( string, excludeTrailingBracket = false )
+{
+  var regex = /\bhttps?:\/\/\S+/gi;
+  
+  var urls = string.match( regex );
+  
+  if( urls !== null )
+  {
+    if( excludeTrailingBracket )
+    {
+      for( var u = 0; u < urls.length; u++ )
+      {
+        if( urls[ u ].substring( urls[ u ].length -1 ) == ")" )
+        {
+          urls[ u ] = urls[ u ].substring( 0, urls[ u ].length -1 );
+        }
+      }
+    }
+    
+    return urls;
+  }
+  return [];
+}
